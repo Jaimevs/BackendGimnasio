f00f8aa4b77dad84c1d5ef7faeabc44bf4871282
Jaimevs <jaime.vqzan@gmail.com>
2025-04-01 00:11:11 -0600

    Modificando modelo evaluaciones servicios

--- models/evaluaciones_serv.py
+++ models/evaluaciones_serv.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column,Boolean, Integer, String, DateTime, ForeignKey, Enum
+from sqlalchemy import Column, Boolean, Integer, String, DateTime, ForeignKey, Enum
 from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
 from config.db import Base
+from datetime import datetime
 import models.persons
 import enum
 
@@ -13,12 +14,14 @@ class Servicios(enum.Enum):
 
 class Evaluaciones_serv(Base):
     __tablename__ = 'tbd_evaluaciones_servicios'
-    ID = Column(Integer, primary_key=True, index=True)
-    Usuario_ID = Column(Integer)
-    Servicios = Column( Enum(Servicios))
-    Calificacion = Column(String(60))
-    Criterio = Column(String(100))
-    Estatus = Column(Boolean)
     
-    Fecha_Registro = Column(DateTime)
+    ID = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    Usuario_ID = Column(Integer, nullable=False)
+    Servicios = Column(Enum(Servicios), nullable=False)
+    Calificacion = Column(String(60), nullable=False)
+    Criterio = Column(String(100), nullable=False)
+    Estatus = Column(Boolean, default=True, nullable=False)
+    Fecha_Registro = Column(DateTime, default=datetime.now, nullable=False)
+    Fecha_Actualizacion = Column(DateTime, nullable=True, onupdate=datetime.now)
+    
     # intems = relationship("Item", back_populates="owner") Clave foranea
