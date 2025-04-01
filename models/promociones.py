d6285b721b0921c51a5c8b8c883510d85ba28879
Jaimevs <jaime.vqzan@gmail.com>
2025-03-31 23:51:28 -0600

    Modificando modelo promociones

--- models/promociones.py
+++ models/promociones.py
@@ -1,7 +1,8 @@
-from sqlalchemy import Column,Boolean, Integer, DateTime, ForeignKey, Enum
+from sqlalchemy import Column, Boolean, Integer, DateTime, ForeignKey, Enum
 from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
 from config.db import Base
+from datetime import datetime
 import enum
 
 class MyTipo(enum.Enum):
@@ -15,11 +16,11 @@ class MyAplicacion(enum.Enum):
 
 class Promocion(Base):
     __tablename__ = 'tbb_promociones'
-    ID = Column(Integer, primary_key=True, index=True)
-    Producto_id = Column(Integer)
+    
+    ID = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    Producto_id = Column(Integer, nullable=False)
     Tipo = Column(Enum(MyTipo), nullable=False)
     Aplicacion_en = Column(Enum(MyAplicacion), nullable=False)
-    Fecha_Registro = Column(DateTime)
-    Fecha_Actualizacion = Column(DateTime)
-    Estatus = Column(Boolean, default=False)
-
+    Fecha_Registro = Column(DateTime, nullable=False, default=datetime.now)
+    Fecha_Actualizacion = Column(DateTime, nullable=True, onupdate=datetime.now)
+    Estatus = Column(Boolean, default=True, nullable=False)
