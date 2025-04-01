57b6d181f1d3390c67341917657b4dc48d10a275
Jaimevs <jaime.vqzan@gmail.com>
2025-04-01 00:03:05 -0600

    Modificacon en modelo rols

--- models/rols.py
+++ models/rols.py
@@ -5,11 +5,12 @@ from datetime import datetime
 
 class Rol(Base):
     __tablename__ = 'tbc_roles'
-    ID = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    Nombre = Column(String(60), unique=True, index=True)
+    
+    ID = Column(Integer, primary_key=True, index=True, autoincrement=True, nullable=False)
+    Nombre = Column(String(60), unique=True, index=True, nullable=False)
     Descripcion = Column(Text, nullable=True)
-    Estatus = Column(SmallInteger, default=1)
-    Fecha_Registro = Column(DateTime, default=datetime.now)
+    Estatus = Column(SmallInteger, default=1, nullable=False)
+    Fecha_Registro = Column(DateTime, default=datetime.now, nullable=False)
     Fecha_Actualizacion = Column(DateTime, nullable=True, onupdate=datetime.now)
     
     # Relación con usuarios
